c7c1e1426bec8eba698421f066efa0b09245a9f1
Tathya <coding.tathya@gmail.com>
2026-04-08 16:17:10 +0530

    scrollbar

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -32,12 +32,47 @@
 
     ctx?.putImageData(imageData, 0, 0);
     document.body.style.backgroundImage = `url(${canvas.toDataURL()})`;
+
+    document.addEventListener("scroll", () => {
+      const scrollbar = document.getElementById("scrollbar");
+      if (!scrollbar) return;
+
+      const scrollTop = window.scrollY;
+      const maxScroll =
+        document.documentElement.scrollHeight - window.innerHeight;
+
+      const progress = maxScroll > 0 ? scrollTop / maxScroll : 0;
+
+      // clamp
+      const p = Math.min(Math.max(progress, 0), 1);
+
+      const start = 33;
+      const end = 66;
+
+      // map to 25vh → 75vh
+      const heightVH = 10; // example height of scrollbar
+      const topVH = start + (end - heightVH - start) * p;
+
+      scrollbar.style.top = `${topVH}vh`;
+      scrollbar.style.height = `${heightVH}vh`;
+    });
   });
 </script>
 
 <svelte:head>
   <link rel="icon" href="/assets/logo.png" />
 </svelte:head>
+
+<div class="h-screen w-screen fixed top-0 left-0 z-10">
+  <div
+    class="w-3 h-[35vh] top-[32vh] right-0 absolute backdrop-blur-sm rounded-l-xl shadow-left-glow"
+  ></div>
+
+  <div
+    class="w-1 h-[10vh] top-[33vh] absolute right-1 rounded-full bg-dark-blue"
+    id="scrollbar"
+  ></div>
+</div>
 
 <Navbar />
 {@render children()}
