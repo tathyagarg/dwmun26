ce0c8665867332f9134bfb30e657734d37a5b7a7
Tathya <coding.tathya@gmail.com>
2026-05-01 12:55:52 +0530

    sec

--- src/routes/secretariat/+page.svelte
+++ src/routes/secretariat/+page.svelte
@@ -73,30 +73,31 @@
 </script>
 
 <div class="w-full flex items-center flex-col gap-4 py-24">
-  <h1 class="text-8xl font-bold">Secretariat</h1>
+  <h1 class="text-4xl sm:text-8xl font-bold">Secretariat</h1>
 
-  <div class="w-2/3 grid grid-cols-6 gap-4">
+  <div
+    class="w-full sm:w-2/3 grid grid-cols-2 sm:grid-cols-6 gap-4 px-4 sm:px-0"
+  >
     {#each sec as s}
       <div
-        class="bg-custom-white p-4 shadow-2xl shadow-black/40 rounded border-1 border-dark-blue"
-        class:col-span-3={s.don}
-        class:col-span-2={!s.don}
-        class:rounded-l-2xl!={s.left}
-        class:rounded-r-2xl!={s.right}
-        class:col-start-2={s.start === 2}
+        class="bg-custom-white col-span-2 shadow-2xl shadow-black/40 rounded border-1 border-dark-blue overflow-hidden"
+        class:sm:col-span-3!={s.don}
+        class:sm:rounded-l-2xl!={s.left}
+        class:sm:rounded-r-2xl!={s.right}
+        class:sm:col-start-2={s.start === 2}
       >
         <img
           src="/assets/sec/{s.name.split(' ')[0].toLowerCase()}.jpg"
           alt={s.name}
-          class="w-full h-auto rounded-lg aspect-square object-cover mb-4"
+          class="w-full h-auto aspect-square object-cover mb-4 border-b-1 border-dark-blue"
         />
         <h2
-          class="text-center text-2xl leading-[2] font-semibold"
-          class:text-4xl!={s.don}
+          class="text-center text-lg sm:text-2xl leading-[2] font-semibold"
+          class:sm:text-4xl!={s.don}
         >
           {s.name}
         </h2>
-        <p class="text-center text-xl">{s.role}</p>
+        <p class="text-center text-base sm:text-xl">{s.role}</p>
       </div>
     {/each}
   </div>
